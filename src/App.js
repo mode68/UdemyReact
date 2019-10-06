01f21847278529766ba8c411e4e46f6a6e6f6dcb
Glasbleidd <mode68@gmai.com>
2019-10-06 11:11:37 +0300

    Section 7 Lesson 84. Removed error boundary components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-//import './App.css';
 import Person from './Person/Person';
 import classes from './App.module.css';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = ({
@@ -32,7 +30,6 @@ class App extends Component {
   };
 
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
@@ -63,13 +60,13 @@ class App extends Component {
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-                  <Person 
-                    click={() => this.deletePersonHandler(index)}
-                    name={person.name} 
-                    age={person.age}
-                    changed={(event) => this.nameChangedHandler(event, person.id)} />
-                </ErrorBoundary>
+            return <Person 
+              key={person.id}
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age}
+              changed={(event) => this.nameChangedHandler(event, person.id)} 
+            />
           })}
         </div>
       );
