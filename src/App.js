92a28035cf363feaf92a93969a48fc7a44646bb5
Glasbleidd <mode68@gmai.com>
2019-10-06 11:08:31 +0300

    Section 6 Lesson 8. Error boundary components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 //import './App.css';
 import Person from './Person/Person';
 import classes from './App.module.css';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = ({
@@ -55,7 +56,6 @@ class App extends Component {
   };
 
   render() {
-
     let persons = null;
     let btnClass = '';
 
@@ -63,12 +63,13 @@ class App extends Component {
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
-            return <Person 
-                key={person.id}
-                click={() => this.deletePersonHandler(index)}
-                name={person.name} 
-                age={person.age}
-                changed={(event) => this.nameChangedHandler(event, person.id)} />
+            return <ErrorBoundary key={person.id}>
+                  <Person 
+                    click={() => this.deletePersonHandler(index)}
+                    name={person.name} 
+                    age={person.age}
+                    changed={(event) => this.nameChangedHandler(event, person.id)} />
+                </ErrorBoundary>
           })}
         </div>
       );
